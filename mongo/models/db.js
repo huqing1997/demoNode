629bf92ee45c298d1b71ce131b71924d2a66fefb
huqing <2853226197@qq.com>
2020-06-15 17:23:18 +0800

    first commit

---
+++ mongo/models/db.js
@@ -0,0 +1,11 @@
+const conf = require('./config')
+const {EventEmitter} = require('events')
+
+// 客户端
+const {MongoClient} = require('mongodb')
+
+class Mongodb {
+    constructor(conf) {
+
+    }
+}
